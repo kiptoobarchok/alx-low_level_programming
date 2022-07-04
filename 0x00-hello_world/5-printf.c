9ca66a5faaeef2d1d4df6183b1d7605a3297b37c
Kiptoobarchok <calebkiptoo9090@gmail.com>
2022-07-05 02:25:31 +0300

    Hello-world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  *main - Entry point
  *
- *Return: 0 
+ *return 0 after program execution
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
