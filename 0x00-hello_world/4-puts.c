c8a24fa3757eba16ef682e24f0308637764d34b3
Kiptoobarchok <calebkiptoo9090@gmail.com>
2022-07-05 01:45:35 +0300

    Hello-world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main = entry point 
+ *main - entry point
  *
- * return :(0) success
- *
+ *Return: 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
