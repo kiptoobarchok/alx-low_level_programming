063aec535738e898fd34f8020e03099478829278
Kiptoobarchok <calebkiptoo9090@gmail.com>
2022-07-05 02:27:10 +0300

    Hello-world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  *
  *return 0 after program execution
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
