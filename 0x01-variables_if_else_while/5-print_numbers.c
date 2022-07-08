29a41175766b2833b5b7400816ea97ca54eba52c
Kiptoobarchok <calebkiptoo9090@gmail.com>
2022-07-08 12:54:14 +0300

    Variables-if-else-while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ *main - prints numbers from base 10
+ *char
+ *return : 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+
+	printf("%i", i);
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
