7dccb84bef2c3ccca747f1a84d8ceec029dae65f
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-05-18 13:32:00 -0700

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - prints "Programming is like building a multilingual puzzle
+  *
+  *Retutn : 0
+  */
+
+int main()
+{
+	char my_string[] = "\"Programming is like building a multilingual puzzle\n";
+
+		puts(my_string);
+
+	return (0);
+}
