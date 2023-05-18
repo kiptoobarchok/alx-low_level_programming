5c6bafb8f8a4df0742668678dbbbce1df4fdc83d
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-05-18 14:20:40 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	long long int too_long;
 	float decimal;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long int: %ld byte(s)\n", sizeof(long_int));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(too_long));
-	printf("size of a float: %ld byte(s)\n", sizeof(decimal));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long_int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(too_long));
+	printf("Size of a float: %ld byte(s)\n", sizeof(decimal));
 
 	return (0);
 }
