0ce9d23d98e2eaf348348370ae6ed612cb376a83
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-05-19 13:26:02 -0700

    Variables if else while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  *main - This function starts the program
+  *
+  *Description: The program prints alphabets
+  *Return: 0
+  */
+
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets > 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
