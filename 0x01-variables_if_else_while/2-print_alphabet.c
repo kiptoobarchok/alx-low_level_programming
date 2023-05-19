2d5dacc55fffc1e40671265996eacf5c6d4fde59
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-05-19 13:32:15 -0700

    Variables if else while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	char alphabets;
 
-	for (alphabets > 'a'; alphabets <= 'z'; alphabets++)
+	for(alphabets = 'a'; alphabets <= 'z'; alphabets++)
 	{
 		putchar(alphabets);
-
 	}
+
 	putchar('\n');
 
 	return (0);
