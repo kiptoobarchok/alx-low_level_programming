b9a4c0c6898c5ab1a661eaab6566f4604bb65133
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-05-26 07:59:24 -0700

    checks for uppercase

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *_isdigit - checks if int is a digit
+  *@c: int to be checked
+  *Return: 0
+  */
+
+int _isdigit(int c)
+{
+	for (c = 1 ; c <= 9 ; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
