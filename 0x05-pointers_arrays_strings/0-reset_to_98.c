ff489aec2786d866083409b0bf703c9dc0c1ff9a
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-05-29 12:02:59 -0700

    updating value of int using a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
-  *reset_to_98 - function to take a pointer to an int 
+  *reset_to_98 - function to take a pointer to an int
   *as parameter and updates the value it points to to 98
-  *@*n: pointer 
+  *@n: integer
   *
   *Return: 0
   */
