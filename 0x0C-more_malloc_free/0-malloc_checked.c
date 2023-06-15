4b2b2d921e1751c792d47849d45d97f3a4e1af9a
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-06-15 14:12:17 -0700

    function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,6 +5,7 @@
 /**
   *malloc_checked- function to allocate memory using malloc
   *@b:size of memory to allocate
+  *Return: pointer to allocated memory
   */
 
 void *malloc_checked(unsigned int b)
