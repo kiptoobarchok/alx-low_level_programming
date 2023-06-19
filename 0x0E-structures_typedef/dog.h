fc447594d0dc3dd5fde241398195e9b8f5096422
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-06-19 13:05:01 -0700

    type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_h
+
+typedef struct dog dog_name;
+/**
+  *struct dog - structure that stores info
+  *@name: dog's  name
+  *@age: age of the dog
+  *@owner: dog's owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_name *new_dog(char *name, float age, char *owner);
+void free_dog(dog_name *d);
+
+#endif
