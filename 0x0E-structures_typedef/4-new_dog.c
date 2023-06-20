2a5afe1abcfaae6ed2ba4621a0664aa924f1cf74
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-06-20 12:37:14 -0700

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,14 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
+	dog_t *new_dog;
 	int i, len_name, len_owner;
 
-	ptr = malloc(sizeof(*ptr));
+	new_dog = malloc(sizeof(*new_dog));
 
-	if (ptr == NULL || !(name) || !(owner))
+	if (new_dog == NULL || !(name) || !(owner))
 	{
-		free(ptr);
+		free(new_dog);
 		return (NULL);
 	}
 	for (len_name = 0; name[len_name] ;  len_name++)
@@ -28,29 +28,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (len_owner = 0; owner[len_owner] ;  len_owner++)
 		;
 
-	ptr->name = malloc(len_name + 1);
-	ptr->owner = malloc(len_owner + 1);
+	new_dog->name = malloc(len_name + 1);
+	new_dog->owner = malloc(len_owner + 1);
 
-	if (!(ptr->name) || !(ptr->owner))
+	if (!(new_dog->name) || !(new_dog->owner))
 	{
-		free(ptr->owner);
-		free(ptr->name);
-		free(ptr);
+		free(new_dog->owner);
+		free(new_dog->name);
+		free(new_dog);
 
 		return (NULL);
 	}
 	for (i = 0; i < len_name; i++)
 	{
-		ptr->name[i] = name[i];
+		new_dog->name[i] = name[i];
 	}
-	ptr->name[i] = '\0';
+	new_dog->name[i] = '\0';
 
 	for (i = 0; i < len_owner; i++)
 	{
-		ptr->owner[i] = owner[i];
-	ptr->owner[i] = '\0';
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
 	}
 
-	ptr->age = age;
-	return (ptr);
+	new_dog->age = age;
+	return (new_dog);
 }
