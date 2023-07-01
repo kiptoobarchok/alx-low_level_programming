f8056a7ffb373bc9c6303bb7297bc27bde8c1b2f
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-07-01 13:57:25 -0700

    task 2: prepend a node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,31 @@
 #include "lists.h"
 
 /**
-  *
-  *
+  *add_node - function to prepend a node
+  *@head: start of linked list
+  *@str: string to append
+  *Return: address of the head
   */
 
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t i;
 
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	new->len = i;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
