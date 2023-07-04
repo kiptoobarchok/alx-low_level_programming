cb80fb686168d8c8a4b0c3635c5ea21b6524acff
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-07-04 12:46:53 -0700

    function to print number of elemnts

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+  *listint_len- function to print no of elemnts
+  *@h: head of linked list
+  *Return: number of elemnts in list
+  */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t no_elements = 0;
