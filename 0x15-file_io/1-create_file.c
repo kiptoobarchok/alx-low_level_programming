6794e62fa7be85502c9308776bc86659296efd6b
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-07-22 06:38:01 +0000

    function to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,18 +12,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int op, wr, count;
 
-	if (filename == NULL)
+	if (!filename)
 	{
 		return (-1);
 	}
 
-	if (text_content != NULL)
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (!text_content)
 	{
-		for (count = 0; text_content[count];)
-			count++;
+		text_content = "";
 	}
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	for (count = 0; text_content[count]; count++)
+		;
+
 	wr = write(op, text_content, count);
 
 	if (op == -1 || wr == -1)
