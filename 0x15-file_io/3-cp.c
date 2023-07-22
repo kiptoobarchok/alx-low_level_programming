a58dce597d1dd9ce94520fed47bea9e884a6912b
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-07-22 07:47:30 +0000

    function that copies content of file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
 	close_err = close(file_to);
 	if (close_err == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
